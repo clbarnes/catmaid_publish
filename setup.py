0107633fbc4592119500a14303ab26bbc0379093
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2022-11-21 11:36:16 +0000

    Initial commit
        Created repository from template.

---
+++ setup.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+from extreqs import parse_requirement_files
+
+with open(Path(__file__).resolve().parent / "README.md") as f:
+    readme = f.read()
+
+install_requires, extras_require = parse_requirement_files(
+    Path(__file__).resolve().parent / "requirements.txt"
+)
+
+setup(
+    name="catmaid_publish",
+    url="https://github.com/clbarnes/catmaid_publish",
+    author="Chris L. Barnes",
+    description="Scripts for publishing data from CATMAID",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=find_packages(
+        where="src",
+        include=["catmaid_publish*"]
+    ),
+    install_requires=install_requires,
+    extras_require=extras_require,
+    python_requires=">=3.9, <4.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+)
---
+++ src/catmaid_publish/__init__.py
@@ -0,0 +1,5 @@
+"""
+# catmaid_publish package
+"""
+from .version import version as __version__  # noqa: F401
+from .version import version_tuple as __version_info__  # noqa: F401
