c65a817bfbbfd5c05902dab98526aa4cdc2e6731
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2023-01-09 10:56:59 +0000

    Refactor navis environment variables

--- src/catmaid_publish/__init__.py
+++ src/catmaid_publish/__init__.py
@@ -1,11 +1,6 @@
 """
 # catmaid_publish package
 """
-import os
-
-os.environ["NAVIS_HEADLESS"] = "true"
-os.environ["NAVIS_SKIP_LOG_SETUP"] = "true"
-
 from .main import publish_from_config
 from .version import version as __version__  # noqa: F401
 from .version import version_tuple as __version_info__  # noqa: F401
