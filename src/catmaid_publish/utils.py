7cfc684b89d5c881425cc459e068a86b3468f13c
Chris Barnes <barnesc@janelia.hhmi.org>
2023-03-20 18:28:45 +0000

    WIP

--- src/catmaid_publish/utils.py
+++ src/catmaid_publish/utils.py
@@ -4,6 +4,7 @@ from copy import copy, deepcopy
 from functools import lru_cache, wraps
 from typing import Optional, TypeVar
 
+import pymaid
 import networkx as nx
 
 logger = logging.getLogger(__name__)
@@ -86,21 +87,33 @@ def copy_cache(deep: bool = True, maxsize: Optional[int] = 128, typed: bool = Fa
     return wrapper
 
 
-def descendants(g: nx.DiGraph, roots: list[str], max_depth=None) -> set[str]:
+def accept(node_id, node_data: dict[str, Any]) -> bool:
+    return True
+
+
+def descendants(g: nx.DiGraph, roots: list[str], max_depth=None, select_fn: Optional[Callable[[Hashable, dict[str, Any]], bool]]=None) -> set[str]:
     """Find all descendant nodes from given roots, to a given depth.
 
     Output includes given roots.
     """
+    if select_fn is None:
+        select_fn = accept
+
     out = set()
     to_visit: list[tuple[str, int]] = [(r, 0) for r in roots]
     while to_visit:
         node, depth = to_visit.pop()
-        if node in out:
+
+        if node in out or not select_fn(node, g.nodes[node]):
             continue
+
         out.add(node)
+
         if max_depth is not None and depth >= max_depth:
             continue
+
         to_visit.extend((n, depth + 1) for n in g.successors(node))
+
     return out
 
 
@@ -123,3 +136,58 @@ def join_markdown(*strings: Optional[str]) -> str:
         out.append(out.pop() + "\n")
 
     return "\n\n---\n\n".join(out)
+
+
+def ancestors(g: nx.DiGraph, leaves):
+    select = set()
+    to_visit = list(leaves)
+    while to_visit:
+        nid = to_visit.pop()
+        if nid in select:
+            continue
+        select.add(nid)
+        to_visit.extend(g.predecessors(nid))
+    return select
+
+
+def tree_annotating_type(g: nx.DiGraph, roots: list[Hashable], etype: str):
+    def fn(nid, d):
+        return d["type"] in ("annotation", etype)
+
+    downward = descendants(g, roots, select_fn=fn)
+    sub = g.subgraph(downward)
+
+    upward = ancestors(sub, leaves)
+    return g.subgraph(downward.intersection(upward))
+
+
+@copy_cache(maxsize=None)
+def entity_subgraphs(neuron_ann=None, volume_ann=None, annotation_ann=None):
+    if neuron_ann is None:
+        neuron_ann = []
+    if volume_ann is None:
+        volume_ann = []
+    if annotation_ann is None:
+        annotation_ann = []
+
+    anns = neuron_ann + volume_ann + annotation_ann
+
+    g = pymaid.get_entity_graph(
+        types=["neuron", "volume", "annotation"],
+        annotated_with=[anns],
+        expand_subannotations=anns,
+    )
+
+    vol_set = set()
+    ann_to_id = {d["name"]: d["id"] for _, d in g.nodes(data=True) if d["type"] == "annotation"}
+
+    if volume_ann:
+        vol_g = tree_annotating_type(g, volume_ann, "volume")
+
+    if neuron_ann:
+        neu_g = tree_annotating_type(g, neuron_ann, "neuron")
+
+    if annotation_ann:
+        ann_g = g.subgraph([n for n, d in g.nodes(data=True) if d["type"] == "annotation"])
+
+    return neu_g, vol_g, ann_g
